8cee1983047841864e83a626be58f1fe2e6b72ec
vidadofael <vidadofael@gmail.com>
2023-04-05 16:58:45 -0300

    Atualizando código para excluir item

--- controller/lista-produtos-controller.js
+++ controller/lista-produtos-controller.js
@@ -1,5 +1,5 @@
 import { produtoService } from "../service/produto-service.js";
-const criaNovaDivProduto = (imagem, nome, preco) => {
+const criaNovaDivProduto = (imagem, nome, preco, id) => {
     const linhaNovoProduto = document.createElement('div');
     linhaNovoProduto.classList.add('item__produto');
     linhaNovoProduto.classList.add('produto-novo');
@@ -7,25 +7,43 @@ const criaNovaDivProduto = (imagem, nome, preco) => {
     const conteudo = `
             <img class="img__produto" src="${imagem}" alt="imagem do produto">
             <p class="nome__produto">${nome}</p>
-            <p class="preco__produto">${preco}</p>
+            <p class="preco__produto">R$ ${preco}</p>
             <a href="../telas/mais-detalhes-produtos.html">Ver mais</a>
             <div class="botoes__gestao">
                 <div id="imagemExcluir" class="btn__excluir">
                     <img class="img__btn-excluir" src="../assets/icone-apagar.png" alt="botao apagar produto">
                 </div>
                 <div id="imagemEditar" class="btn__editar">
-                    <img class="img__btn-editar" src="../assets/icone-editar.png" alt="botao editar item">
+                    <a href="../telas/editar-produto.html?id=${id}">
+                        <img class="img__btn-editar" src="../assets/icone-editar.png" alt="botao editar item">
+                    </a>
                 </div>
             </div>
         `
     linhaNovoProduto.innerHTML = conteudo
+    linhaNovoProduto.dataset.id = id
+    console.log(linhaNovoProduto)
+
     return linhaNovoProduto
 }
 
 const sessao = document.querySelector('[data-section]') /*data-tabela*/
 
+sessao.addEventListener('click', (evento) => {
+    let ehBotaoDeletar = evento.target.className === 'img__btn-excluir';
+    if (ehBotaoDeletar) {
+      const linhaNovoProduto = evento.target.closest('[data-id]');
+      let id = linhaNovoProduto.dataset.id;
+      if (confirm("Tem certeza que deseja excluir este produto?")) {
+        produtoService.removeProduto(id);
+      } else {
+        console.log("Exclusão cancelada pelo usuário");
+      }
+    }
+});
+
 produtoService.listaProduto() /*itera e exibe dados na tela que foram coletados pela fetch*/
 .then(data => {
     data.forEach(elemento => {
-        sessao.appendChild(criaNovaDivProduto(elemento.imagem, elemento.nome, elemento.preco))
+        sessao.appendChild(criaNovaDivProduto(elemento.imagem, elemento.nome, elemento.preco, elemento.id))
 })})
