cdfd677e561bbff05ac2004ba93113444e367fa1
vidadofael <vidadofael@gmail.com>
2023-04-06 10:57:05 -0300

    tentando servicos de edição e exclusao

--- controller/cadastra-produtos-controller.js
+++ controller/cadastra-produtos-controller.js
@@ -1,10 +1,13 @@
 import { produtoService } from "../service/produto-service.js"
 const formulario = document.querySelector('[data-form]')
 formulario.addEventListener('submit', (evento) => {
+    console.log('Evento submit disparado!');
     evento.preventDefault()
     const imagem = evento.target.querySelector('[data-imagem]').value
     const nome = evento.target.querySelector('[data-nome]').value
     const preco = evento.target.querySelector('[data-preco]').value
+
+    console.log(`imagem: ${imagem}, nome: ${nome}, preco: ${preco}`);
 
     produtoService.criaProduto(imagem, nome, preco)
     .then(() => {
